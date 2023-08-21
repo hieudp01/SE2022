8c8890966c481b143c0a358dd5b5ee211b3e03e8
hieudp01 <hieuqh5@gmail.com>
2023-08-21 19:47:42 +0700

    Change

--- controller/authentication/utils.py
+++ controller/authentication/utils.py
@@ -2,6 +2,7 @@ import bcrypt
 from flask import Blueprint, render_template, request, session, redirect, url_for
 
 from model.role import Role
+from model.Teacher import Teacher
 
 
 def hash_password(password):
@@ -11,13 +12,19 @@ def hash_password(password):
 def verify_password(password, hashed_password):
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
+def verify_password_no_hash(password, db_pass):
+    return password == db_pass
+
 
 def login(user, col, role):
     if request.form.get("username") is None or request.form.get("password") is None:
         return render_template('authentication/login.html', role=role, error="Username or password is empty")
 
+    print(request.form['username'])
+    print(request.form['password'])
+
     user = user.query.where(col == request.form['username']).first()
-    if user is None or not verify_password(request.form['password'], user.password):
-        return render_template('authentication/login.html', role=role,
-                               error="Username or password is incorrect")
+    
+    if user is None or not verify_password_no_hash(request.form['password'], user.password):
+        return render_template('authentication/login.html', role=role, error="Username or password is incorrect")
     return user
--- db_config.py
+++ db_config.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-connection_string = "mysql+mysqlconnector://root:@localhost:3306/seedschool"
+connection_string = "mysql+mysqlconnector://root:123456@localhost:3306/seedschool"
 engine = create_engine(connection_string, echo=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
