832b04d2c1cebc083eac91dd629ec6f3e1e9070e
stewie2k2 <n.hghuy1311@gmail.com>
2023-08-21 17:27:00 +0700

    change db_config

--- db_config.py
+++ db_config.py
@@ -1,7 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from urllib.parse import quote_plus
 
-connection_string = "mysql+mysqlconnector://root:@localhost:3306/seedschool"
+password = quote_plus("123456aA@") 
+
+connection_string = f"mysql+mysqlconnector://root:{password}@localhost:3306/seedschool"
 engine = create_engine(connection_string, echo=True)
 db_session = scoped_session(
     sessionmaker(autocommit=False, autoflush=False, bind=engine)
